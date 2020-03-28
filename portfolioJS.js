6403426e5fc88dbae0acd82f72aa7fa2f37b61e1
sigur0r <artyom.schukin@gmail.com>
2020-03-28 20:01:40 +0300

    major updates

---
+++ portfolioJS.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+$(document).ready(function(){
+    $("#page-header").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
