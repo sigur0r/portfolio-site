4a7abb0413314b42a28c9ce9d6b7632b9788f89b
Artyom Schukin <37814017+sigur0r@users.noreply.github.com>
2020-04-01 12:25:35 +0300

    Update textAppearAnim.js

--- textAppearAnim.js
+++ textAppearAnim.js
@@ -3,8 +3,3 @@ window.sr = new ScrollReveal();
 ScrollReveal().reveal('.page-header', { delay: 500 });
 ScrollReveal().reveal('.heading', { delay: 500 });
 ScrollReveal().reveal('.subhead', { delay: 500 });
-ScrollReveal().reveal('.subheading',);
-ScrollReveal().reveal('.container',);
-ScrollReveal().reveal('.user-image',);
-ScrollReveal().reveal('.skills-list',);
-ScrollReveal().reveal('.portfolio',);
