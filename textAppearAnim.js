119baa2695891d843afb7715ce9dcf0dac25156c
Artyom Schukin <37814017+sigur0r@users.noreply.github.com>
2020-04-02 12:10:25 +0300

    Update textAppearAnim.js

--- textAppearAnim.js
+++ textAppearAnim.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
 window.sr = new ScrollReveal();
 
 ScrollReveal().reveal('.page-header', { delay: 500 });
 ScrollReveal().reveal('.heading', { delay: 500 });
 ScrollReveal().reveal('.subhead', { delay: 500 });
-  )};
