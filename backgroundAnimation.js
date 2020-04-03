583061b09bc2c38369254b0247948eae6341795d
sigur0r <37814017+sigur0r@users.noreply.github.com>
2020-04-03 17:18:13 +0300

    попытка сделать классный фон

---
+++ backgroundAnimation.js
@@ -0,0 +1,13 @@
+window._ = require('vanta.fog.min');
+window._ = require('three.r95.min');
+VANTA.FOG({
+    el: "header",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x2f2812,
+    midtoneColor: 0xebebeb,
+    lowlightColor: 0x3b12fa,
+    baseColor: 0x3cabd9
+});
