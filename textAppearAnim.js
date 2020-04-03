5cbe255fd4a1612ef3ee4ea7cddcf4ea59e9e21b
sigur0r <37814017+sigur0r@users.noreply.github.com>
2020-04-03 20:05:19 +0300

    фикс

--- textAppearAnim.js
+++ textAppearAnim.js
@@ -1,5 +1,5 @@
 window.sr = new ScrollReveal();
 
-ScrollReveal().reveal('.page-header', { delay: 500 });
-ScrollReveal().reveal('.heading', { delay: 500 });
-ScrollReveal().reveal('.subhead', { delay: 500 });
+ScrollReveal().reveal('.page-header', { delay: 1500 });
+ScrollReveal().reveal('.heading', { delay: 1500 });
+ScrollReveal().reveal('.subhead', { delay: 1500 });
