223a701e380f71673df026b3f003d2f1840fc1d4
Artyom Schukin <37814017+sigur0r@users.noreply.github.com>
2020-04-25 21:20:47 +0300

    Create polufillFix.js

---
+++ polufillFix.js
@@ -0,0 +1,10 @@
+var getPixelRatio = function(context) {
+    var backingStore = context.backingStorePixelRatio ||
+          context.webkitBackingStorePixelRatio ||
+          context.mozBackingStorePixelRatio ||
+          context.msBackingStorePixelRatio ||
+          context.oBackingStorePixelRatio ||
+          context.backingStorePixelRatio || 1;
+
+    return (window.devicePixelRatio || 1) / backingStore;
+};
