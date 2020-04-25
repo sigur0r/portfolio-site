b85d521f588639345e6f7590ee604cbe8f89e868
Artyom Schukin <37814017+sigur0r@users.noreply.github.com>
2020-04-25 21:03:54 +0300

    Update backgroundAnimation.js

--- backgroundAnimation.js
+++ backgroundAnimation.js
@@ -1,15 +1,13 @@
 window.onload = function () {
-    VANTA.NET({
+   VANTA.FOG({
         el: "header",
         mouseControls: true,
         touchControls: true,
         minHeight: 200.00,
         minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00,
-        color: 0x3fddff,
-        backgroundColor: 0x115da0,
-        points: 11.00,
-        spacing: 16.003
+        highlightColor: 0x2f2812,
+        midtoneColor: 0xebebeb,
+        lowlightColor: 0x3b12fa,
+        baseColor: 0x3cabd9
     });
 };
