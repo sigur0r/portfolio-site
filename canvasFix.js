58ba996864ec24e6d265ee903a1696f937ee356a
Artyom Schukin <37814017+sigur0r@users.noreply.github.com>
2020-04-27 21:46:43 +0300

    Update canvasFix.js

--- canvasFix.js
+++ canvasFix.js
@@ -1,16 +1,36 @@
-$(function() {
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+export default function scaleCanvas(canvas, context, width, height) {
+  // assume the device pixel ratio is 1 if the browser doesn't specify it
+  const devicePixelRatio = window.devicePixelRatio || 1;
 
-if (window.devicePixelRatio > 1) {
-    var canvasWidth = canvas.width;
-    var canvasHeight = canvas.height;
+  // determine the 'backing store ratio' of the canvas context
+  const backingStoreRatio = (
+    context.webkitBackingStorePixelRatio ||
+    context.mozBackingStorePixelRatio ||
+    context.msBackingStorePixelRatio ||
+    context.oBackingStorePixelRatio ||
+    context.backingStorePixelRatio || 1
+  );
 
-    canvas.width = canvasWidth * window.devicePixelRatio;
-    canvas.height = canvasHeight * window.devicePixelRatio;
-    canvas.style.width = canvasWidth;
-    canvas.style.height = canvasHeight;
+  // determine the actual ratio we want to draw at
+  const ratio = devicePixelRatio / backingStoreRatio;
 
-    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+  if (devicePixelRatio !== backingStoreRatio) {
+    // set the 'real' canvas size to the higher width/height
+    canvas.width = width * ratio;
+    canvas.height = height * ratio;
+
+    // ...then scale it back down with CSS
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+  }
+  else {
+    // this is a normal 1:1 device; just scale it simply
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.width = '';
+    canvas.style.height = '';
+  }
+
+  // scale the drawing context so everything will work at the higher ratio
+  context.scale(ratio, ratio);
 }
-})
