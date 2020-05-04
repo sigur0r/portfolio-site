2eb526a9ed33aa5e07ada40d0ce133d9d3b854b8
sigur0r <artyom.schukin@gmail.com>
2020-05-04 10:19:34 +0300

    background update

--- backgroundAnimation.js
+++ backgroundAnimation.js
@@ -1,13 +1,12 @@
 window.onload = function () {
-   VANTA.FOG({
+   VANTA.NET({
         el: ".header-bg",
         mouseControls: true,
-        touchControls: true,
-        minHeight: 750.00,
-        minWidth: 1334.00,
-        highlightColor: 0x2f2812,
-        midtoneColor: 0xebebeb,
-        lowlightColor: 0x3b12fa,
-        baseColor: 0x3cabd9
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x967bb,
+        backgroundColor: 0x379af2
     });
 };
