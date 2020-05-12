e0f33aca36dbdf84ab091b3e34753bd70a959f8c
sigur0r <37814017+sigur0r@users.noreply.github.com>
2020-05-12 23:25:41 +0300

    анимировал раздел skillsRow

--- dist/textAppearAnim.js
+++ dist/textAppearAnim.js
@@ -3,3 +3,8 @@ window.sr = new ScrollReveal();
 ScrollReveal().reveal('.page-header', { delay: 1500 });
 ScrollReveal().reveal('.heading', { delay: 1500 });
 ScrollReveal().reveal('.subhead', { delay: 1500 });
+ScrollReveal().reveal('#htmlSkill', { delay: 500 });
+ScrollReveal().reveal('#cssSkill', { delay: 600 });
+ScrollReveal().reveal('#JSSkill', { delay: 700 });
+ScrollReveal().reveal('#JQSkill', { delay: 800 });
+ScrollReveal().reveal('#SassSkill', { delay: 900 });
